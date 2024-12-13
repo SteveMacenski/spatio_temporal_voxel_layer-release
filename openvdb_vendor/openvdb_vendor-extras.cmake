e7caae90a749c3a34821bc054efd4df4b42d35fd
stevemacenski <stevenmacenski@gmail.com>
2024-12-13 21:27:52 +0000

    Trimmed the branch to only the spatio_temporal_voxel_layer sub directory

--- openvdb_vendor/openvdb_vendor-extras.cmake
+++
@@ -1,7 +0,0 @@
-# Default to Release build
-if(NOT CMAKE_BUILD_TYPE OR CMAKE_BUILD_TYPE STREQUAL "")
-  set(CMAKE_BUILD_TYPE "Release" CACHE STRING "" FORCE)
-endif()
-message( STATUS "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}" )
-list(APPEND CMAKE_MODULE_PATH "${openvdb_vendor_DIR}/../../../opt/openvdb_vendor/lib/cmake/OpenVDB")
-find_package(OpenVDB REQUIRED)
